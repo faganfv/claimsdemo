0597b556e0c74dd855d7876ed8f972f29751c43f
faganfv <faganfv@gmail.com>
2026-01-09 15:01:07 -0500

    Initial commit

---
+++ src/main/java/com/claims/demo/ClaimsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.claims.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaimsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/claims/demo/ClaimsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claims.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
